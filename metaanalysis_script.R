219aec0ccc827bb02c2dde0cca8383c8dca4ff05
AleTeran <didi_127@hotmail.com>
2017-10-23 17:09:53 -0500

    Update metaanalysis_script.R

--- metaanalysis_script.R
+++ metaanalysis_script.R
@@ -1,5 +1,5 @@
 #This script was used to the data analysis in the paper: 
-#"A comparative meta-analysis profiling reveals differences in phenotypic traits among domesticated, genetically engineered and wild plants relatives of rice, canola, maize, pumpkin and sunflower.  Hernández-Terán, Alejandra; Wegier, Ana; Benítez, Mariana; Lira, Rafael; and Escalante, Ana*”
+#"Domesticated, genetically engineered and wild plants relatives exhibit unintended phenotypic differences: a comparative meta-analysis profiling of rice, canola, maize, pumpkin and sunflower.  Terán, Alejandra H.; Wegier, Ana; Benítez, Mariana; Lira, Rafael; and Escalante, Ana*”
 #*Corresponding author: aescalante@iecologia.unam.mx
 
 #Normality of the data
